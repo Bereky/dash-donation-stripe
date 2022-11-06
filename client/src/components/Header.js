023632f21004de40d62ac90314530410ed7eca49
bereky <bereketlemma4@gmail.com>
2022-11-06 19:05:12 +0300

    link &  brand position update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,16 @@
 
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return ( 
-        <header className="container h-20 shadow-sm relative flex-row w-auto">
-            <div className="absolute h-20 flex justify-center items-center w-full shadow-md ">
-                <h2 className="text-3xl font-extrabold text-orangeMain cursor-pointer">DashDonation</h2>
+        <header className="h-20 shadow-sm relative flex-row w-100">
+            <div className="absolute h-20 flex justify-center items-center w-full shadow-md sm:justify-start">
+                <Link to="/">
+                    <h2 className="text-3xl font-extrabold text-orangeMain cursor-pointer ml-5">DashDonation</h2>
+                </Link>
             </div>
-                <div className="absolute w-48 h-20 right-0 flex justify-center items-center">
-                    <button className="w-3/4 h-10 rounded-2xl border-solid border-2 text-white bg-orangeMain border-orangeMain hover:text-orangeMain hover:bg-white text-lg font-medium  duration-300">Donate</button>
+                <div className="absolute w-48 h-20 right-0 flex justify-center items-center ">
+                    <button className="w-3/4 h-10 rounded-2xl border-solid border-2 text-white bg-orangeMain border-orangeMain hover:text-orangeMain hover:bg-white text-lg font-medium  duration-300 hidden sm:block">Donate</button>
                 </div>
         </header>
      );
